c72da9076657fe39067b5578a4522873ebd90634
Miguel Wilson Cruz <m@navegawireless.com>
2016-08-24 22:22:07 +0000

    my filter ls

---
+++ filter_dir_list.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var path = require('path');
+
+function filteredLs() {
+
+  var extension = "." + process.argv[3].toString();
+
+  fs.readdir(process.argv[2], function doneReading(error, dirList) {
+    if (error) return console.error(error);
+    var list = dirList.filter( function (f) {
+      if (path.extname(f) == extension) return f;
+    });
+   list = list.join().replace(/,/g, "\n");
+   console.log(list);
+
+  } );
+
+}
+
+filteredLs();
