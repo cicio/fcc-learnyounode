f1ffec3b390bf38a2e0bf6403918e2a308b9b6e7
Miguel Wilson Cruz <m@navegawireless.com>
2016-08-24 22:23:51 +0000

    Other filter ls

--- filter_dir_list.js
+++ filter_dir_list.js
@@ -18,3 +18,19 @@ function filteredLs() {
 }
 
 filteredLs();
+
+
+//Solution from learnyounode
+//var fs = require('fs');
+
+//function filteredLs() {
+//  var extension = "." + process.argv[3].toString();
+//  fs.readdir(process.argv[2], function doneReading(error, dirList) {
+//    if (error) return console.error(error);
+//    var list = dirList.filter( function (file) {
+//      path.extname(file) == extension;
+//    });
+//    console.log(list)  
+//  } );
+//}
+
